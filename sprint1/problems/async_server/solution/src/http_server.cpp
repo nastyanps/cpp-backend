720838430e2fae962d876a31a744919444f9ef8e
Anastasia Napasnyuk <yfcnz23062005@mail.ru>
2026-06-28 15:10:40 +0300

    add async_server solution

---
+++ sprint1/problems/async_server/solution/src/http_server.cpp
@@ -0,0 +1,8 @@
+#include "http_server.h"
+
+#include <boost/asio/dispatch.hpp>
+#include <iostream>
+
+namespace http_server {
+	// Реализация вынесена в заголовочный файл (шаблонные классы)
+}  // namespace http_server
